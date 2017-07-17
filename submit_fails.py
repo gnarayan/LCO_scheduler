a8c91c241262c66d6c2126cc594aaecd5c7afa60
Gautham Narayan <gsnarayan@gmail.com>
2017-07-17 11:26:20 -0400

    simple script to resubmit a few jobs which validated but failed on submission because of ipp_value

---
+++ submit_fails.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+import sys
+import glob
+import json
+import requests
+import warnings
+import astropy.units as u
+
+if __name__=='__main__':
+    exptime = 0                           
+    token = sys.argv[1]
+    f = glob.glob('LCO_json/SDSSJ235144*json')
+    f = sorted(f)
+    f = f[6:]
+    for infile in f:
+        with open(infile, 'r') as indata:
+            a = json.load(indata)
+        a['ipp_value'] = 1.0
+        resp_validate = requests.post('https://observe.lco.global/api/userrequests/validate/',\
+		         headers={'Authorization': 'Token {}'.format(token)}, json=a).json()
+        if len(resp_validate['errors']) == 0:
+            exptime += resp_validate['request_durations']['duration']
+            resp_submit = requests.post('https://observe.lco.global/api/userrequests/',\
+		     headers={'Authorization': 'Token {}'.format(token)}, json=a)
+	    try:
+    		resp_submit.raise_for_status()
+	    except requests.exceptions.HTTPError as exc:
+                print resp_submit.json()
+            	message = 'Request failed: {}'.format(infile)
+            	warnings.warn(message)
+	else:
+            print resp_validate
+	    print 'Not submitted: {}'.format(infile)
+
+    exptime*= u.second
+    left = exptime - 48*u.hour
+    print exptime, left
+
