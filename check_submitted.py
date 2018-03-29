510d4000911de25ead41c7f1636d0292bdc5ef8f
Gautham Narayan <gsnarayan@gmail.com>
2018-03-29 15:11:13 -0400

    update for 2018A

--- check_submitted.py
+++ check_submitted.py
@@ -2,11 +2,12 @@
 import sys
 import requests
 import numpy as np
+import astropy.table as at
 
 if __name__=='__main__':
-    targets = np.recfromtxt('targets_LCO2017AB_002.txt', names=True)
+    targets = at.Table.read('targets_LCO2018A_002.txt', format='ascii')
     headers={'Authorization': 'Token {}'.format(sys.argv[1])}
-    for x in targets.targetname:
-        obs = requests.get('https://observe.lco.global/api/userrequests/?proposal=LCO2017AB-002&title={}'.format(x.split('.')[0]),headers=headers).json()
+    for x in targets['targetname']:
+        obs = requests.get('https://observe.lco.global/api/userrequests/?proposal=LCO2018A-002&title={}'.format(x.split('.')[0]),headers=headers).json()
         for y in obs['results']:
-            print y['group_id']
+            print(y['group_id'])
